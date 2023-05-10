448febfa200cc4723243816fada52d9150190aaf
trinhdinhvu2312 <dragonking023@gmail.com>
2023-05-10 21:15:46 +0700

    buoi2

---
+++ m4u/src/m4u/M4u.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m4u;
+
+/**
+ *
+ * @author student
+ */
+public class M4u {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
